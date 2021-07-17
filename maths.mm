d9697f096954fb1c36a93078b534d93e7dbf8582
unknown <omgolatkar2001@gmail.com>
2021-07-17 16:13:34 +0530

    first commit

---
+++ maths.mm
@@ -0,0 +1,186 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="maths" FOLDED="false" ID="ID_1983557763" CREATED="1626273575516" MODIFIED="1626273657121" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.597">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="1" OBJECT="java.lang.Long|1" POSITION="right" ID="ID_1026994206" CREATED="1626273622272" MODIFIED="1626273630058">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="2" OBJECT="java.lang.Long|2" POSITION="right" ID="ID_1221110769" CREATED="1626273630729" MODIFIED="1626273632401">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="3" OBJECT="java.lang.Long|3" POSITION="right" ID="ID_707594644" CREATED="1626273632815" MODIFIED="1626273633833">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="4" OBJECT="java.lang.Long|4" POSITION="right" ID="ID_1584950896" CREATED="1626273634136" MODIFIED="1626273634985">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="5 and 7" POSITION="left" ID="ID_1625483806" CREATED="1626273653111" MODIFIED="1626273683568" HGAP_QUANTITY="40.249999217689066 pt" VSHIFT_QUANTITY="-52.499998435378124 pt">
+<edge COLOR="#7c0000"/>
+<node TEXT="rings and modules (4)" FOLDED="true" ID="ID_1560261034" CREATED="1626274299253" MODIFIED="1626274338768">
+<node TEXT="0149.jpg" ID="ID_872653917" CREATED="1626274346590" MODIFIED="1626274346595">
+<hook URI="maths/0149.jpg" SIZE="0.06837575" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="real analysis 2 (4)" FOLDED="true" ID="ID_1985803601" CREATED="1626274359621" MODIFIED="1626274389816">
+<node TEXT="0150.jpg" ID="ID_1632596196" CREATED="1626274702186" MODIFIED="1626274702192">
+<hook URI="maths/0150.jpg" SIZE="0.06579945" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="point set topology (4)" FOLDED="true" ID="ID_1138742431" CREATED="1626274391950" MODIFIED="1626274404471">
+<node TEXT="0151.jpg" ID="ID_1887754238" CREATED="1626274716451" MODIFIED="1626274716454">
+<hook URI="maths/0151.jpg" SIZE="0.110549346" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="ordinary differential equations (4)" FOLDED="true" ID="ID_582758638" CREATED="1626274405005" MODIFIED="1626274439488">
+<node TEXT="0152.jpg" ID="ID_1667049412" CREATED="1626274730716" MODIFIED="1626274730720">
+<hook URI="maths/0152.jpg" SIZE="0.09983458" NAME="ExternalObject"/>
+</node>
+<node TEXT="0153.jpg" ID="ID_1063913263" CREATED="1626274750021" MODIFIED="1626274750021">
+<hook URI="maths/0153.jpg" SIZE="0.09493468" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="graph theory (4)" FOLDED="true" ID="ID_1599248989" CREATED="1626274440365" MODIFIED="1626274451623">
+<node TEXT="0154.jpg" ID="ID_1605155245" CREATED="1626274783052" MODIFIED="1626274783056">
+<hook URI="maths/0154.jpg" SIZE="0.06871952" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="numerical analysis (4)" FOLDED="true" ID="ID_703108778" CREATED="1626274452133" MODIFIED="1626274469342">
+<node TEXT="0155.jpg" ID="ID_440811887" CREATED="1626274793162" MODIFIED="1626274793166">
+<hook URI="maths/0155.jpg" SIZE="0.0881101" NAME="ExternalObject"/>
+</node>
+<node TEXT="0156.jpg" ID="ID_1159090617" CREATED="1626274801626" MODIFIED="1626274801631">
+<hook URI="maths/0156.jpg" SIZE="0.09358041" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="fields and galois theory (4)" FOLDED="true" ID="ID_1120081151" CREATED="1626274469765" MODIFIED="1626274486928">
+<node TEXT="0157.jpg" ID="ID_3997296" CREATED="1626274811452" MODIFIED="1626274811455">
+<hook URI="maths/0157.jpg" SIZE="0.07825214" NAME="ExternalObject"/>
+</node>
+<node TEXT="0158.jpg" ID="ID_1014264182" CREATED="1626274827212" MODIFIED="1626274827212">
+<hook URI="maths/0158.jpg" SIZE="0.079660356" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="markov chains and queueing models (4)" FOLDED="true" ID="ID_1206958189" CREATED="1626274499005" MODIFIED="1626274516887">
+<node TEXT="0159.jpg" ID="ID_439416272" CREATED="1626274843291" MODIFIED="1626274843295">
+<hook URI="maths/0159.jpg" SIZE="0.072388984" NAME="ExternalObject"/>
+</node>
+<node TEXT="0160.jpg" ID="ID_633702190" CREATED="1626274850772" MODIFIED="1626274850777">
+<hook URI="maths/0160.jpg" SIZE="0.070750564" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="only 7" POSITION="left" ID="ID_1625083470" CREATED="1626274542214" MODIFIED="1626274546903">
+<edge COLOR="#007c00"/>
+<node TEXT="algebraic topology (4)" FOLDED="true" ID="ID_435273127" CREATED="1626274548598" MODIFIED="1626274561543">
+<node TEXT="0167.jpg" ID="ID_1967778986" CREATED="1626274913764" MODIFIED="1626274913769">
+<hook URI="maths/0167.jpg" SIZE="0.08146133" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="algorithms (4)" FOLDED="true" ID="ID_872391226" CREATED="1626274567741" MODIFIED="1626274574144">
+<node TEXT="0168.jpg" ID="ID_1729208905" CREATED="1626274921126" MODIFIED="1626274921130">
+<hook URI="maths/0168.jpg" SIZE="0.08653908" NAME="ExternalObject"/>
+</node>
+<node TEXT="0169.jpg" ID="ID_1018513132" CREATED="1626274935594" MODIFIED="1626274935595">
+<hook URI="maths/0169.jpg" SIZE="0.07957439" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="probability (4)" FOLDED="true" ID="ID_859248813" CREATED="1626275028324" MODIFIED="1626285791552" TEXT_SHORTENED="true">
+<node TEXT="0170.jpg" ID="ID_691902509" CREATED="1626275057659" MODIFIED="1626275057663">
+<hook URI="maths/0170.jpg" SIZE="0.06689289" NAME="ExternalObject"/>
+</node>
+<node TEXT="0171.jpg" ID="ID_13948466" CREATED="1626275069733" MODIFIED="1626275069737">
+<hook URI="maths/0171.jpg" SIZE="0.066511564" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="measure theory and integration (4)" FOLDED="true" ID="ID_1441864659" CREATED="1626274586021" MODIFIED="1626274610927">
+<node TEXT="0172.jpg" ID="ID_1862370256" CREATED="1626275086198" MODIFIED="1626275086203">
+<hook URI="maths/0172.jpg" SIZE="0.07221576" NAME="ExternalObject"/>
+</node>
+<node TEXT="0173.jpg" ID="ID_908111705" CREATED="1626275092770" MODIFIED="1626275092774">
+<hook URI="maths/0173.jpg" SIZE="0.066511564" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="probabilistic number theory (4)" FOLDED="true" ID="ID_975983094" CREATED="1626274593309" MODIFIED="1626274627591">
+<node TEXT="0174.jpg" ID="ID_1303003152" CREATED="1626275103986" MODIFIED="1626275103990">
+<hook URI="maths/0174.jpg" SIZE="0.06721059" NAME="ExternalObject"/>
+</node>
+<node TEXT="0175.jpg" ID="ID_31313797" CREATED="1626275110426" MODIFIED="1626275110432">
+<hook URI="maths/0175.jpg" SIZE="0.066781916" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="representation theory of finite groups (4)" FOLDED="true" ID="ID_1029877002" CREATED="1626274637254" MODIFIED="1626274653983">
+<node TEXT="0176.jpg" ID="ID_1762712529" CREATED="1626275120050" MODIFIED="1626275120054">
+<hook URI="maths/0176.jpg" SIZE="0.0574706" NAME="ExternalObject"/>
+</node>
+<node TEXT="0177.jpg" ID="ID_625995794" CREATED="1626275132266" MODIFIED="1626275132271">
+<hook URI="maths/0177.jpg" SIZE="0.056771573" NAME="ExternalObject"/>
+</node>
+<node TEXT="0178.jpg" ID="ID_1849730334" CREATED="1626275137890" MODIFIED="1626275137893">
+<hook URI="maths/0178.jpg" SIZE="0.061506405" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</node>
+</map>
